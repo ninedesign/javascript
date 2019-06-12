a07df8cdf6a9294d01975d9d033ed695dbc8eb30
Stefano Bossi <nineusa@gmail.com>
2019-06-12 10:53:19 -0600

    6-11-19 class files update
    Same as before just replace these files inside the s'src' folder - the glitch in the Api.js file was just a missing curly bracket :)

---
+++ react-6-11-19/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// for the form app
+// import App from './App';
+// for the API call app
+import App from './Api';
+
+ReactDOM.render(<App />, document.getElementById('root'));
